0be59d05f63fbfab4754fb1e6d0c24dbf159ba22
klarulor <klarulor@gmail.com>
2022-12-04 14:41:48 +0200

    fix: [200~object is possibly undefined~ in example

--- example/index.ts
+++ example/index.ts
@@ -1,15 +1,18 @@
-import {RentryClient, Paste} from "rentry-pastebin";
+import {RentryClient, Paste, IPasteStruct} from "rentry-pastebin";
 
 const client = new RentryClient();
 (async () => {
     await client.createToken();
     console.log(`New token is: ${client.getToken()}`);
     const struct = await client.createPaste({content: "Idk some words maybe", customEditCode: "imsostupid"})
-    console.log(`Paste url is: "https://rentry.co/${struct.url}" and secret edit key is "${struct.editCode}" and content is "${struct.paste.getContent()}"`);
-    //paste.paste.pull(); // Pull or update content from rentry.co
-    setTimeout(() => {
-        struct.paste.setContent("new content; New message xD");
-        console.log(`Updated content. Check the website`);
-    }, 5000);
 
+    if(struct.paste){
+        console.log(`Paste url is: "https://rentry.co/${struct.url}" and secret edit key is "${struct.editCode}" and content is "${struct.paste.getContent()}"`);
+        //paste.paste.pull(); // Pull or update content from rentry.co
+        setTimeout(() => {
+            if(struct.paste)
+                struct.paste.setContent("new content; New message xD");
+            console.log(`Updated content. Check the website`);
+        }, 5000);
+    }
 })()
